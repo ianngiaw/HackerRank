d31ec5cadb0d816bcebb03d7201a6aa916f19300
Ian Ngiaw <ianngiawtingan@gmail.com>
2017-05-06 13:56:53 -0700

    Update solution

--- Quicksort1/solution.py
+++ Quicksort1/solution.py
@@ -1,14 +1,18 @@
-n = input()
-arr = map(int, input().split(' '))
+m = input()
+array = map(int, input().split(' '))
 
-pivot, curr = 0, 1
-while pivot < n and curr < n:
-  if arr[pivot] > arr[curr]:
-    temp_pivot = arr[pivot]
-    arr[pivot] = arr[curr]
-    arr[curr] = arr[pivot + 1]
-    arr[pivot + 1] = temp_pivot
-    pivot += 1
-  curr += 1
+def partition(arr):
+  n = len(arr)
+  pivot, curr = 0, 1
+  while pivot < n and curr < n:
+    if arr[pivot] > arr[curr]:
+      temp_pivot = arr[pivot]
+      arr[pivot] = arr[curr]
+      arr[curr] = arr[pivot + 1]
+      arr[pivot + 1] = temp_pivot
+      pivot += 1
+    curr += 1
 
-print(' '.join(map(str, arr)))
+  return arr
+
+print(' '.join(map(str, partition(array))))
